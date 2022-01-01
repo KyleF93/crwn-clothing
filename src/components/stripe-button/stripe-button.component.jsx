9e7468cf3d72c8e4abda5719e1751d9cbec8be24
Kyle Fox <Kylexz@icloud.com>
2022-01-01 20:34:15 +0000

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KDDkYDhdfoYTdqpSo7LE4wkJ22qsl6HQPRg1s0x7VB81nu4Ay9VXvmOESG67sHXwtxFgNhYzODWh8AcEz3e6Jzz003yI6YeiJ";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
