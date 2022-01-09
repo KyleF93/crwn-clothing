c1897b63e41cfddea35fd10a5515f414829551ff
Kyle Fox <Kylexz@icloud.com>
2022-01-09 17:23:24 +0000

    adding styled components, done cart-icon item, checkout-item, collection-item & removed scss imports from checkout & header

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart-selector";
-import "./cart-icon.styles.scss";
 import { createStructuredSelector } from "reselect";
 
 import {
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-
-import "./collection-item.styles.scss";
-import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
+import {
+  CollectionItemButton,
+  CollectionItemContainer,
+  CollectionItemFooter,
+  CollectionItemImage,
+  CollectionItemName,
+  CollectionItemPrice,
+} from "./collection-item.styles";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
   return (
-    <div className="collection-item">
-      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">${price}</span>
-      </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+    <CollectionItemContainer>
+      <CollectionItemImage style={{ backgroundImage: `url(${imageUrl})` }} />
+      <CollectionItemFooter>
+        <CollectionItemName>{name}</CollectionItemName>
+        <CollectionItemPrice>${price}</CollectionItemPrice>
+      </CollectionItemFooter>
+      <CollectionItemButton onClick={() => addItem(item)} inverted>
         Add to cart
-      </CustomButton>
-    </div>
+      </CollectionItemButton>
+    </CollectionItemContainer>
   );
 };
 
